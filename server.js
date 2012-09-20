159f4ea6c6ecb6f014eb170e5143afd8f0274e5d
root <root@tnwt-ThinkPad-Edge.(none)>
2012-09-20 18:13:54 +0700

    Send 404 when request not exists

--- server.js
+++ server.js
@@ -3,14 +3,36 @@ var mime = require('mime');
 var fs = require('fs');
 
 var server = http.createServer(function(req, resp) {
-	var filePath = './public/index.html';
-	fs.readFile(filePath, function(err, data) {
-		var mimeType = mime.lookup(filePath);
-		resp.writeHead('content-type', mimeType);
-		resp.end(data);
-	});
+	var url = req.url;
+	var filePath;
+	if(url == '/') {
+		filePath = './public/index.html';
+	} else {
+		filePath = './public' + url;
+	}
+	
+	fs.exists(filePath, function(exists) {
+		if(exists) {
+			fs.readFile(filePath, function(err, data) {
+				var mimeType = mime.lookup(filePath);
+				resp.writeHead('content-type', mimeType);
+				resp.end(data);
+			});
+		} else {
+			send404(resp);
+		}
+	})
+
+	
 });
 
 server.listen(3000, function() {
 	console.log('server start');
 });
+
+function send404(resp) {
+	resp.writeHead(404, {
+		'content-type': 'text/plain'
+	});
+	resp.end('404');
+}
